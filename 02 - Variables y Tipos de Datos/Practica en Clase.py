7e4827ed24745839a59e0d46eb00895d8fdf53c1
Facundo Marino <facundomarino1@outlook.com>
2023-02-23 23:01:17 -0300

    hii

--- 02 - Variables y Tipos de Datos/Practica en Clase.py
+++ 02 - Variables y Tipos de Datos/Practica en Clase.py
@@ -1,12 +1,12 @@
 mi_variable = 12
 print(mi_variable)
 
-mi_variable2 = 'dario'
+mi_variable2 = 'Facundo'
 print(mi_variable2)
 
 mi_complejo = 5 + 7j
-print(mi_complejo)
+print('el numero complejo es ',mi_complejo)
 
 
-mi_numero = input('Ingrese un valor')
-print('el valor ingresado fue', mi_numero)
+mi_numero = input('Ingrese su nombre: ')
+print('es gay', mi_numero*3)
